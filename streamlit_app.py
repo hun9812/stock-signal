195e27df97234f614b575f8a287ad7e01d06b6a2
hun9812 <ihyh1231@naver.com>
2025-06-26 18:40:16 +0900

    ascending = False

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ import requests
 API_URL = "https://stock-signal-35232a1d0473.herokuapp.com/signals/"
 
 st.title("📈 주식 시그널 검색기")
-ticker = st.text_input("🔍 티커를 입력하세요 (예: AAPL, TSLA)")
+ticker = st.text_input("🔍 티커를 입력하세요 (예: AAPL, TSLA), 대소문자 구분 없음")
 
 if st.button("시그널 조회") and ticker:
     url = API_URL + ticker.upper()
@@ -17,6 +17,7 @@ if st.button("시그널 조회") and ticker:
         data = response.json()
         df = pd.DataFrame(data)
         df["Date"] = pd.to_datetime(df["Date"])
+        df = df.sort_values(by="Date", ascending=False)
         st.dataframe(df)
     else:
         st.error(f"❌ 오류: {response.json().get('detail', 'API 호출 실패')}")
